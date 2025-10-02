1579dd2e0cdd235fe0fbe36f67dec9af863fa7e9
CodeX047 <vishalrp047@gmail.com>
2025-10-02 22:25:12 +0530

    Cleanup for Navbar

--- src/components/landing/NavBar.jsx
+++ src/components/landing/NavBar.jsx
@@ -1,16 +1,6 @@
 import React from "react";
-import {
-  Heart,
-  MessageCircle,
-  MapPin,
-  Clock,
-  Users,
-  Shield,
-  ArrowRight,
-  BriefcaseMedical,
-} from "lucide-react";
+import { BriefcaseMedical } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import "./../../../Constant/index.jsx";
 
 const onEnterApp = () => {
   window.location.href = "/home";

ae1c078f8ab460db9d653625251f73c954e5ebb0
CodeX047 <vishalrp047@gmail.com>
2025-10-03 18:55:26 +0530

    Import Trust to Page

--- src/components/landing/Page.jsx
+++ src/components/landing/Page.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from './NavBar'
 import Hero from './Hero'
 import Feature from './Feature'
+import Trust from './Trust'
 
 const Page = () => {
   
@@ -10,6 +11,7 @@ const Page = () => {
       <NavBar />
       <Hero />
       <Feature />
+      <Trust />
     </main>
   )
 }

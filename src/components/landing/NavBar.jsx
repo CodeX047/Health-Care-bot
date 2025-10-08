1b9cf1d34c5f6ef4092364cecae8490e8908501b
CodeX047 <vishalrp047@gmail.com>
2025-10-08 21:29:57 +0530

    Add SMS to navbar use LINK

--- src/components/landing/NavBar.jsx
+++ src/components/landing/NavBar.jsx
@@ -8,13 +8,24 @@ const NavBar = () => {
       <div className="max-w-6xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-3">
-            <img src="/image/logo.png" alt="Health Care Bot" className="w-40 sm:w-[150px]" />
+            <img
+              src="/image/logo.png"
+              alt="Health Care Bot"
+              className="w-40 sm:w-[150px]"
+            />
           </div>
-          <Link to="/chat">
-            <Button className="px-4 py-2 bg-primary text-white rounded-md cursor-pointer">
-              Launch App
-            </Button>
-          </Link>
+          <div>
+            <Link to="/chat">
+              <Button className="px-4 py-2 bg-primary text-white rounded-md cursor-pointer">
+                Launch App
+              </Button>
+            </Link>
+            <Link to={"/sms"}>
+              <Button className="ml-3 px-4 py-2 bg-primary text-white rounded-md cursor-pointer">
+                Open SMS App
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
